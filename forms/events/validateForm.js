9fec005371735d6bb32e69ec9a7b3bff0cda2b9a
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2023-09-04 19:28:01 +0000

    Adição do Validate

--- forms/events/validateForm.js
+++ forms/events/validateForm.js
@@ -2,6 +2,18 @@ function validateForm(form){
 	
 	if(form.getValue("ramal") == "") {
 		throw "Campo Ramal não foi preenchido.";
-    }
-    
+	}
+	if(form.getValue("obs") == "") {
+		throw "Campo Ramal não foi preenchido.";
+	}
+	if(form.getValue("txt_categoria") == "") {
+		throw "Campo Ramal não foi preenchido.";
+	}
+	if(form.getValue("maquina") == "") {
+		throw "Campo Ramal não foi preenchido.";
+	}
+	if(form.getValue("ramal") == "") {
+		throw "Campo Ramal não foi preenchido.";
+	}
+	
 }
