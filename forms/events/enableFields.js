574d62dfeed8b30b3b98dd6c1b5dbd70b9922f85
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2023-09-11 18:50:14 +0000

    Bloqueio dos Campos 4

--- forms/events/enableFields.js
+++ forms/events/enableFields.js
@@ -2,14 +2,21 @@ function enableFields(form) {
 	var Now_State = parseInt(getValue("WKNumState"));
 	form.setEnabled("name", false);
 
-//	---Analisar Chamado || Atender Chamado---
-	
+	//	---Analisar Chamado || Atender Chamado---
+
 	if (Now_State == 6 || Now_State == 9) {
 		form.setEnabled("ramal", false);
 		form.setEnabled("maquina", false);
 		form.setEnabled("tipo", false);
 		form.setEnabled("txt_categoria", false);
 		form.setEnabled("tit", false);
-		form.setEnabled("obs", false);
+		form.setEnabled("descricao", false);
+	}
+	if (Now_State == 4) {
+		form.setEnabled("ramal", false);
+		form.setEnabled("maquina", false);
+		form.setEnabled("tit", false);
+		form.setEnabled("descricao", false);
+		form.setEnabled("info", false);
 	}
 }
