bfb663e8493f137a31ce1dd410405bb0ad9b0934
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2023-09-15 17:36:54 +0000

    Função para Prazo

--- forms/custom.js
+++ forms/custom.js
@@ -1,3 +1,5 @@
+//    Função de Avaliação
+
 $(function () {
     FLUIGC.stars("#avaliacao_stars", {
         stars: 5,
@@ -8,11 +10,22 @@ $(function () {
         $("#avaliacao").val(index);
     });
 });
+ 
 
-//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var tipoServicoSelect = document.getElementById("tipoServico");
 var tipoCatInput = document.getElementById("tipoCat");
 var tipoSLAInput = document.getElementById("tipoSLA");
+var tipoDataSomadaInput = document.getElementById("tipoDataSomada");
+
+
+//    Função para Pular Data
+
+function ajustarParaProximoDiaUtil(data) {
+    while (data.getDay() === 6 || data.getDay() === 0) {
+        data.setDate(data.getDate() + 1);
+    }
+}
 
 tipoServicoSelect.addEventListener("change", function() {
     var selectedServico = tipoServicoSelect.value;
@@ -22,10 +35,28 @@ tipoServicoSelect.addEventListener("change", function() {
     for (var i = 0; i < dataset.values.length; i++) {
         var row = dataset.values[i];
         if (row.Servico === selectedServico) {
-            tipoCatInput.value = row["Categoria"];
-            tipoSLAInput.value = row["SLA"];
+            var hora = parseInt(row["SLA"]); 
+            var dataAtual = new Date(); 
+            dataAtual.setHours(dataAtual.getHours() + hora); 
+
+            // Chama a função para ajustar para o próximo dia útil
+            ajustarParaProximoDiaUtil(dataAtual);
+
+            tipoSLAInput.value = hora; 
+            tipoCatInput.value = row["Categoria"]; 
+
+            // Formate a data como "dd-mm-aaaa"
+            var dia = String(dataAtual.getDate()).padStart(2, '0'); 
+            var mes = String(dataAtual.getMonth() + 1).padStart(2, '0'); 
+            var ano = dataAtual.getFullYear(); 
+            var dataFormatada = dia + '-' + mes + '-' + ano;
+            tipoDataSomadaInput.value = dataFormatada;
+
             break;
         }
     }
 });
-//
+
+
+
+
