60cf3548e2e6cb33368ca2a442250103364a7731
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2023-09-21 15:24:21 -0400

    Ajuste nas Regras de Validação

--- forms/events/validateForm.js
+++ forms/events/validateForm.js
@@ -1,5 +1,6 @@
 function validateForm(form){
-	
+	var Now_State = parseInt(getValue("WKNumState"));
+
 	if(form.getValue("ramal") == "") {
 		throw "Campo Ramal não foi preenchido.";
 	}
@@ -12,5 +13,28 @@ function validateForm(form){
 	if(form.getValue("maquina") == "") {
 		throw "Campo Número da Máquina não foi preenchido.";
 	}
+	if(form.getValue("titulo") == "") {
+		throw "Campo Título não foi preenchido.";
+	}
 
+	// ---Analisar Chamado - Suporte Geral---
+
+	if (Now_State == 10) {
+		if(form.getValue("equipamento") == "") {
+			throw "Campo Equipamento não foi preenchido.";
+		}
+		if(form.getValue("valorEstimado") == "") {
+			throw "Campo Valor Estimado não foi preenchido.";
+		}
+	}
+
+	//	---Avaliar Chamado---
+	if (Now_State == 14) {
+		if(form.getValue("avaliacao") == "") {
+			throw "Campo Avaliação não foi preenchido.";
+		}
+		if(form.getValue("obs1") == "") {
+			throw "Campo Observação não foi preenchido.";
+		}
+	}
 }
