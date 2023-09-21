35d5c2f537adb62f0b3dcaf3dcd76486a802ba29
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2023-09-21 13:31:32 -0400

    Atualização DisplayFields

--- forms/events/displayFields.js
+++ forms/events/displayFields.js
@@ -8,19 +8,6 @@ function displayFields(form, customHTML) {
 	form.setHidePrintLink(true);
 	form.setValue("dataAval", data.format(new Date()));
 	
-	//	---Avaliar Chamado---
-	if (Now_State == 14) {
-		form.setVisibleById("paineldesc", false);
-		form.setVisibleById("painel_req", false);
-		form.setVisibleById("p1", false);
-		form.setVisibleById("p25", false);
-		form.setVisibleById("p50", false);
-		form.setVisibleById("p75", false);
-		form.setVisibleById("inputAdicionarSoluc", false);
-		form.setVisibleById("painel_aprov", false);
-		form.setVisibleById("painel_desc", false);
-		form.setVisibleById("inputAdicionar", false);
-	}
 	//  Analisar Chamado - Todos
 	if (Now_State == 12 || Now_State == 8 || Now_State == 6) {
 		form.setVisibleById("painel_sup", false);
@@ -99,4 +86,18 @@ function displayFields(form, customHTML) {
 		form.setVisibleById("aproval", false);
 		form.setVisibleById("tabela_Rateio", false);
 	}
+
+		//	---Avaliar Chamado---
+		if (Now_State == 14) {
+			form.setVisibleById("paineldesc", false);
+			form.setVisibleById("painel_req", false);
+			form.setVisibleById("p1", false);
+			form.setVisibleById("p25", false);
+			form.setVisibleById("p50", false);
+			form.setVisibleById("p75", false);
+			form.setVisibleById("inputAdicionarSoluc", false);
+			form.setVisibleById("painel_aprov", false);
+			form.setVisibleById("painel_desc", false);
+			form.setVisibleById("inputAdicionar", false);
+		}
 }	
